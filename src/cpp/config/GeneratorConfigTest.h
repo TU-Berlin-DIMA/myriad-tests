e8123ee3f9cdaa562b98139d3dc9f17fac22d241
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2012-05-27 20:11:05 +0200

    Fixed license header in some test-package files, updated myriad-toolkit library

--- src/cpp/config/GeneratorConfigTest.h
+++ src/cpp/config/GeneratorConfigTest.h
@@ -1,10 +1,19 @@
 /*
- * This file is part of the myriad-dgen package.
+ * Copyright 2010-2011 DIMA Research Group, TU Berlin
  *
- * (c) 2010 Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Alexander Alexandrov <alexander.alexandrov@tu-berlin.de>
  */
 
 #ifndef GENERATORCONFIGTEST_H_
