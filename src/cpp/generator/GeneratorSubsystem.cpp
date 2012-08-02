a665c7862709b6be19fd8611342f5fd261b2094c
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2012-08-02 02:44:20 +0200

    Fixed compile errors caused by outdated GeneratorSubsystem implementation

--- src/cpp/generator/GeneratorSubsystem.cpp
+++ src/cpp/generator/GeneratorSubsystem.cpp
@@ -5,9 +5,24 @@ using namespace Poco;
 
 namespace Myriad {
 
+// the initial stage ID should always be zero
+I32u RecordGenerator::Stage::NEXT_STAGE_ID = 0;
+
+// register the valid stages for the Myriad generator extension
+RecordGenerator::StageList initList()
+{
+    RecordGenerator::StageList tmp;
+
+    tmp.push_back(RecordGenerator::Stage("card"));
+
+    return tmp;
+}
+
+const RecordGenerator::StageList RecordGenerator::STAGES(initList());
+
+// register the record sequence generators
 void GeneratorSubsystem::registerGenerators()
 {
-    // register generators with the autorelease pool
 }
 
 } // namespace Myriad
