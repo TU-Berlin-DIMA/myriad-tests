1fcd8fee263972bd8117b0597f314e75655c0494
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2012-08-11 14:27:38 +0200

    Implemented unit tests for ClusteredValueProvider with ConstRangeProvider cardinality provider.

--- src/cpp/math/probability/util/CombinedPrFunctionInput.h
+++ src/cpp/math/probability/util/CombinedPrFunctionInput.h
@@ -21,6 +21,8 @@
 
 #include "math/probability/CombinedPrFunction.h"
 
+#include <Poco/RefCountedObject.h>
+
 #include <cmath>
 
 using namespace Poco;
@@ -28,238 +30,256 @@ using namespace std;
 
 namespace Myriad {
 
-template<typename T> class CombinedPrFunctionInput
+template<typename T> class CombinedPrFunctionInput : public RefCountedObject
 {
 public:
 
-	CombinedPrFunctionInput(double nullProbability, T testRangeMin, T testRangeMax) :
-		_nullProbability(nullProbability),
-		_testRange(testRangeMin, testRangeMax)
-	{
-		_domain.set(numericLimits<T>::max(), numericLimits<T>::min());
-	}
+    CombinedPrFunctionInput(double nullProbability, T testRangeMin, T testRangeMax) :
+        _nullProbability(nullProbability),
+        _testRange(testRangeMin, testRangeMax)
+    {
+        _domain.set(numericLimits<T>::max(), numericLimits<T>::min());
+    }
 
-	CombinedPrFunctionInput(const CombinedPrFunctionInput& o) :
-		_nullProbability(o._nullProbability),
-		_buckets(o._buckets),
-		_exactValueBuckets(o._exactValueBuckets),
-		_multiValueBuckets(o._multiValueBuckets),
-		_pdf(o._pdf),
-		_cdf(o._cdf),
-		_domain(o._domain)
-	{
-	}
+    CombinedPrFunctionInput(const CombinedPrFunctionInput& o) :
+        _nullProbability(o._nullProbability),
+        _buckets(o._buckets),
+        _exactValueBuckets(o._exactValueBuckets),
+        _multiValueBuckets(o._multiValueBuckets),
+        _pdf(o._pdf),
+        _cdf(o._cdf),
+        _domain(o._domain)
+    {
+    }
 
-	T testRangeMin()
-	{
-		return _testRange.min();
-	}
+    T testRangeMin()
+    {
+        return _testRange.min();
+    }
 
-	T testRangeMax()
-	{
-		return _testRange.max();
-	}
+    T testRangeMax()
+    {
+        return _testRange.max();
+    }
 
-	CombinedPrFunctionInput<T>& add(double probability, const T& x)
-	{
-		size_t i = _buckets.size();
+    CombinedPrFunctionInput<T>& add(double probability, const T& x)
+    {
+        size_t i = _buckets.size();
 
-		_buckets.push_back(Interval<T>(x, x+1));
-		_pdf.push_back(probability);
-		_cdf.push_back(i > 0 ? _cdf[i-1] + probability : probability);
+        _buckets.push_back(Interval<T>(x, x+1));
+        _pdf.push_back(probability);
+        _cdf.push_back(i > 0 ? _cdf[i-1] + probability : probability);
 
-		_exactValueBuckets.push_back(i);
+        _exactValueBuckets.push_back(i);
 
-		_domain.set(min(_domain.min(), x), max(_domain.max(), x+1));
+        _domain.set(min(_domain.min(), x), max(_domain.max(), static_cast<T>(x+1)));
 
-		return *this;
-	}
+        return *this;
+    }
 
-	CombinedPrFunctionInput<T>& add(double probability, const T& l, const T& u)
-	{
-		size_t i = _buckets.size();
+    CombinedPrFunctionInput<T>& add(double probability, const T& l, const T& u)
+    {
+        size_t i = _buckets.size();
 
-		_buckets.push_back(Interval<T>(l, u));
-		_pdf.push_back(probability);
-		_cdf.push_back(i > 0 ? _cdf[i-1] + probability : probability);
+        _buckets.push_back(Interval<T>(l, u));
+        _pdf.push_back(probability);
+        _cdf.push_back(i > 0 ? _cdf[i-1] + probability : probability);
 
-		_multiValueBuckets.push_back(i);
+        _multiValueBuckets.push_back(i);
 
-		_domain.set(min(_domain.min(), l), max(_domain.max(), u));
+        _domain.set(min(_domain.min(), l), max(_domain.max(), u));
 
-		return *this;
-	}
+        return *this;
+    }
 
-	double pdf(const T& x)
-	{
-		if (nullValue<T>() == x)
-		{
-			return _nullProbability;
-		}
-		else if (_domain.contains(x))
-		{
-			size_t i = lookup(x);
-			return _pdf[i]/static_cast<double>(_buckets[i].length());
-		}
-		else
-		{
-			return 0.0;
-		}
-	}
+    double pdf(const T& x)
+    {
+        if (nullValue<T>() == x)
+        {
+            return _nullProbability;
+        }
+        else if (_domain.contains(x))
+        {
+            size_t i = lookup(x);
+            return _pdf[i]/static_cast<double>(_buckets[i].length());
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
 
-	double cdf(const T& x)
-	{
-		if (_domain.min() > x)
-		{
-			return 0.0;
-		}
-		else if (_domain.max() <= x)
-		{
-			return 1.0 - _nullProbability;
-		}
-		else
-		{
-			size_t i = lookup(x);
-			if (i > 0)
-			{
-				return _cdf[i-1] + _pdf[i] * (x + 1 - _buckets[i].min())/static_cast<double>(_buckets[i].length());
-			}
-			else
-			{
-				return _pdf[i] * (x + 1 - _buckets[i].min())/static_cast<double>(_buckets[i].length());
-			}
-		}
-	}
+    double cdf(const T& x)
+    {
+        if (_domain.min() > x)
+        {
+            return 0.0;
+        }
+        else if (_domain.max() <= x)
+        {
+            return 1.0 - _nullProbability;
+        }
+        else
+        {
+            size_t i = lookup(x);
+            if (i > 0)
+            {
+                return _cdf[i-1] + _pdf[i] * (x + 1 - _buckets[i].min())/static_cast<double>(_buckets[i].length());
+            }
+            else
+            {
+                return _pdf[i] * (x + 1 - _buckets[i].min())/static_cast<double>(_buckets[i].length());
+            }
+        }
+    }
 
-	std::istream& serialize()
-	{
-		_in.clear();
+    std::istream& serialize()
+    {
+        _in.clear();
 
-		_in << "@numberofexactvals = " << _exactValueBuckets.size()  << "\n"
-			<< "@numberofbins = "      << _multiValueBuckets.size()  << "\n"
-			<< "@nullprobability = "   << _nullProbability           << "\n";
+        _in << "@numberofexactvals = " << _exactValueBuckets.size()  << "\n"
+            << "@numberofbins = "      << _multiValueBuckets.size()  << "\n"
+            << "@nullprobability = "   << _nullProbability           << "\n";
 
-		_in << "# some empty lines and lines with comments\n"
-			<< "\t\n"
-			<< " \t\n"
-			<< " \t\n"
-			<< "\t \n"
-			<< "\t \t\t \t   \t # comment here \n";
+        _in << "# some empty lines and lines with comments\n"
+            << "\t\n"
+            << " \t\n"
+            << " \t\n"
+            << "\t \n"
+            << "\t \t\t \t   \t # comment here \n";
 
-		_in << "# exact value probabilities\n";
-		for (size_t j = 0; j < _exactValueBuckets.size(); j++)
-		{
-			size_t i = _exactValueBuckets[j];
-			_in << "p(X) = " << _pdf[i] << "    for X = { " << _buckets[i].min() << " }\n";
-		}
+        _in << "# exact value probabilities\n";
+        for (size_t j = 0; j < _exactValueBuckets.size(); j++)
+        {
+            size_t i = _exactValueBuckets[j];
+            _in << "p(X) = " << _pdf[i] << "    for X = { " << _buckets[i].min() << " }\n";
+        }
 
-		_in << "# bucket probabilities\n";
-		for (size_t j = 0; j < _multiValueBuckets.size(); j++)
-		{
-			size_t i = _multiValueBuckets[j];
-			_in << "p(X) = " << _pdf[i] << "    for X = { x in [" << _buckets[i].min() << ", " << _buckets[i].max() << ") }\n";
-		}
+        _in << "# bucket probabilities\n";
+        for (size_t j = 0; j < _multiValueBuckets.size(); j++)
+        {
+            size_t i = _multiValueBuckets[j];
+            _in << "p(X) = " << _pdf[i] << "    for X = { x in [" << _buckets[i].min() << ", " << _buckets[i].max() << ") }\n";
+        }
 
-		return _in;
-	}
+        return _in;
+    }
 
-	static CombinedPrFunctionInput<T>* factory()
-	{
-		// Unsupported base parameter type T
-		throw std::exception();
-	}
+    static CombinedPrFunctionInput<T>* factory()
+    {
+        // Unsupported base parameter type T
+        throw std::exception();
+    }
 
 private:
 
-	size_t lookup(const T& x)
-	{
-		// we assert that the value x is in the [_min, _max] range
-		int min = 0;
-		int max = _buckets.size() - 1;
-		int mid = 0;
+    size_t lookup(const T& x)
+    {
+        // we assert that the value x is in the [_min, _max] range
+        int min = 0;
+        int max = _buckets.size() - 1;
+        int mid = 0;
 
-		// continue searching while [min, max] is not empty
-		while (max >= min)
-		{
-			// calculate the midpoint for roughly equal partition //
-			mid = (min + max) / 2;
+        // continue searching while [min, max] is not empty
+        while (max >= min)
+        {
+            // calculate the midpoint for roughly equal partition //
+            mid = (min + max) / 2;
 
-			// determine which subarray to search
-			if (_buckets[mid].max() <=  x)
-			{
-				// change min index to search upper subarray
-				min = mid + 1;
-			}
-			else if (_buckets[mid].min() > x)
-			{
-				// change max index to search lower subarray
-				max = mid - 1;
-			}
-			else
-			{
-				// key found at index mid
-				return mid;
-			}
-		}
+            // determine which subarray to search
+            if (_buckets[mid].max() <=  x)
+            {
+                // change min index to search upper subarray
+                min = mid + 1;
+            }
+            else if (_buckets[mid].min() > x)
+            {
+                // change max index to search lower subarray
+                max = mid - 1;
+            }
+            else
+            {
+                // key found at index mid
+                return mid;
+            }
+        }
 
-		return mid;
-	}
+        return mid;
+    }
 
-	typedef Interval<T> TInterval;
+    typedef Interval<T> TInterval;
 
-	double _nullProbability;
+    double _nullProbability;
 
-	std::vector<TInterval> _buckets;
+    std::vector<TInterval> _buckets;
 
-	std::vector<size_t> _exactValueBuckets;
+    std::vector<size_t> _exactValueBuckets;
 
-	std::vector<size_t> _multiValueBuckets;
+    std::vector<size_t> _multiValueBuckets;
 
-	std::vector<double> _pdf;
+    std::vector<double> _pdf;
 
-	std::vector<double> _cdf;
+    std::vector<double> _cdf;
 
-	TInterval _domain;
+    TInterval _domain;
 
-	TInterval _testRange;
+    TInterval _testRange;
 
-	std::stringstream _in;
+    std::stringstream _in;
 };
+
+template<> CombinedPrFunctionInput<I16u>* CombinedPrFunctionInput<I16u>::factory()
+{
+    CombinedPrFunctionInput<I16u>* input = new CombinedPrFunctionInput<I16u>(0.04000, 0, 100);
+
+    input->add(0.100, 10, 29);
+    input->add(0.100, 29);
+    input->add(0.100, 30, 34);
+    input->add(0.100, 34);
+    input->add(0.100, 35, 43);
+    input->add(0.100, 43);
+    input->add(0.100, 44);
+    input->add(0.100, 45, 59);
+    input->add(0.100, 59);
+    input->add(0.100, 60, 90);
+
+    return input;
+}
 
 template<> CombinedPrFunctionInput<I64u>* CombinedPrFunctionInput<I64u>::factory()
 {
-	CombinedPrFunctionInput<I64u>* input = new CombinedPrFunctionInput<I64u>(0.04000, 0, 100);
+    CombinedPrFunctionInput<I64u>* input = new CombinedPrFunctionInput<I64u>(0.04000, 0, 100);
 
-	input->add(0.1900, 10, 29);
-	input->add(0.0700, 29);
-	input->add(0.0400, 30, 34);
-	input->add(0.0100, 34);
-	input->add(0.0800, 35, 43);
-	input->add(0.0500, 43);
-	input->add(0.0500, 44);
-	input->add(0.1400, 45, 59);
-	input->add(0.0300, 59);
-	input->add(0.3000, 60, 90);
+    input->add(0.1900, 10, 29);
+    input->add(0.0700, 29);
+    input->add(0.0400, 30, 34);
+    input->add(0.0100, 34);
+    input->add(0.0800, 35, 43);
+    input->add(0.0500, 43);
+    input->add(0.0500, 44);
+    input->add(0.1400, 45, 59);
+    input->add(0.0300, 59);
+    input->add(0.3000, 60, 90);
 
-	return input;
+    return input;
 }
 
 template<> CombinedPrFunctionInput<Date>* CombinedPrFunctionInput<Date>::factory()
 {
-	CombinedPrFunctionInput<Date>* input = new CombinedPrFunctionInput<Date>(0.04000, Date("1992-04-01"), Date("1992-07-15"));
+    CombinedPrFunctionInput<Date>* input = new CombinedPrFunctionInput<Date>(0.04000, Date("1992-04-01"), Date("1992-07-15"));
 
-	input->add(0.1900, "1992-04-10", "1992-04-29");
-	input->add(0.0700, "1992-04-29");
-	input->add(0.0400, "1992-04-30", "1992-05-04");
-	input->add(0.0100, "1992-05-04");
-	input->add(0.0800, "1992-05-05", "1992-05-13");
-	input->add(0.0500, "1992-05-13");
-	input->add(0.0500, "1992-05-14");
-	input->add(0.1400, "1992-05-15", "1992-05-29");
-	input->add(0.0300, "1992-05-29");
-	input->add(0.3000, "1992-05-30", "1992-06-29");
+    input->add(0.1900, "1992-04-10", "1992-04-29");
+    input->add(0.0700, "1992-04-29");
+    input->add(0.0400, "1992-04-30", "1992-05-04");
+    input->add(0.0100, "1992-05-04");
+    input->add(0.0800, "1992-05-05", "1992-05-13");
+    input->add(0.0500, "1992-05-13");
+    input->add(0.0500, "1992-05-14");
+    input->add(0.1400, "1992-05-15", "1992-05-29");
+    input->add(0.0300, "1992-05-29");
+    input->add(0.3000, "1992-05-30", "1992-06-29");
 
-	return input;
+    return input;
 }
 
 } // namespace Myriad
--- src/cpp/runtime/provider/value/ClusteredValueProviderTest.h
+++ src/cpp/runtime/provider/value/ClusteredValueProviderTest.h
@@ -19,8 +19,12 @@
 #ifndef CLUSTEREDVALUEPROVIDERTEST_H_
 #define CLUSTEREDVALUEPROVIDERTEST_H_
 
+#include "math/probability/util/CombinedPrFunctionInput.h"
 #include "record/mock/MockRecordA.h"
 #include "runtime/provider/value/ClusteredValueProvider.h"
+#include "runtime/provider/value/ContextFieldValueProvider.h"
+
+#include <bits/stream_iterator.h>
 
 #include <cppunit/TestCaller.h>
 #include <cppunit/TestFixture.h>
@@ -38,6 +42,18 @@ class ClusteredValueProviderTest: public TestFixture
 {
 public:
 
+    typedef RecordTraits<MockRecordA> RecordTraitsType;
+    typedef RecordTraitsType::MetaType RecordMetaType;
+    typedef RecordTraitsType::FactoryType RecordFactoryType;
+    typedef ConstValueProvider<I16u, MockRecordA> I16uConstValueProvider;
+    typedef ConstValueProvider<I64u, MockRecordA> I64uConstValueProvider;
+    typedef ConstValueProvider<Decimal, MockRecordA> DecimalConstValueProvider;
+    typedef FieldSetter<MockRecordA, RecordTraitsType::MOCK_FIELD_1, I16uConstValueProvider> MockField1Setter;
+    typedef FieldSetter<MockRecordA, RecordTraitsType::MOCK_FIELD_2, I64uConstValueProvider> MockField2Setter;
+    typedef FieldSetter<MockRecordA, RecordTraitsType::MOCK_FIELD_3, DecimalConstValueProvider> MockField3Setter;
+    typedef CombinedPrFunction<I16u> PrFunctionType;
+    typedef CombinedPrFunctionInput<I16u> PrFunctionInputType;
+
     void setUp()
     {
         srand(54352364);
@@ -45,10 +61,86 @@ public:
 
     void testClusteredValueProviderWithConstRangeProviderForCardinality()
     {
+        RecordMetaType recordMeta(4096);
+        RecordFactoryType recordFactory(recordMeta);
+
         AutoPtr<MockRecordA> mockCxtRecordPtr;
         RandomStream mockRandom;
 
-        // TODO: implement
+        PrFunctionInputType* prFunctionInput = CombinedPrFunctionInput<I16u>::factory();
+        PrFunctionType* prFunction(new PrFunctionType(prFunctionInput->serialize()));
+
+        I64u scale = 4;
+
+        I64u min = 0;
+        I64u max = power(10, scale);
+        ConstRangeProvider<I64u, MockRecordA> constRangeProvider(min, max);
+
+        ClusteredValueProvider<RecordFieldTraits<RecordTraitsType::MOCK_FIELD_1, MockRecordA>::FieldType, MockRecordA, PrFunctionType, ConstRangeProvider<I64u, MockRecordA> > clusteredValueProvider(*prFunction, constRangeProvider);
+
+        // test range parameters
+        I16u testRangeMin = prFunctionInput->testRangeMin();
+        I16u testRangeMax = prFunctionInput->testRangeMax();
+        I32u testRangeLength = testRangeMax - testRangeMin;
+
+        // a vector for the computed absolute frequencies
+        vector<I64u> absoluteFrequencies(testRangeLength+1, 0);
+        vector<bool> clusterProcessed(testRangeLength+1, false);
+
+        I16u lastValue = nullValue<I16u>();
+        for (I64u i = min; i < max; i++)
+        {
+            mockCxtRecordPtr = recordFactory(i);
+            I16u currValue = clusteredValueProvider(mockCxtRecordPtr, mockRandom);
+
+            // check range
+            CPPUNIT_ASSERT_MESSAGE("Value out of range", (currValue >= testRangeMin && currValue < testRangeMax) || (currValue == nullValue<I16u>()));
+
+            // update absolute frequency count
+            if (currValue == nullValue<I16u>())
+            {
+                absoluteFrequencies[testRangeLength]++;
+            }
+            else
+            {
+                absoluteFrequencies[currValue - testRangeMin]++;
+            }
+
+            // mark previous value cluster as processed
+            if (lastValue != nullValue<I16u>() && lastValue != currValue)
+            {
+                CPPUNIT_ASSERT_MESSAGE(format("Value cluster for value %hu is already processed", currValue), !clusterProcessed[lastValue - testRangeMin]);
+                clusterProcessed[lastValue - testRangeMin] = true;
+            }
+            else if (currValue == nullValue<I16u>() && i == max - 1)
+            {
+                CPPUNIT_ASSERT_MESSAGE(format("Value cluster for value %hu is already processed", currValue), !clusterProcessed[lastValue - testRangeMin]);
+                clusterProcessed[testRangeMax] = true;
+            }
+
+            // update last currValue
+            lastValue = currValue;
+        }
+
+        // check whether all value clusters are processed
+        for (I16u X = testRangeMin; X < testRangeMax + 1U; X++)
+        {
+            if (absoluteFrequencies[X - testRangeMin] == 0)
+            {
+                continue;
+            }
+
+            CPPUNIT_ASSERT_MESSAGE(format("Cluster for value %hu is not processed", (X == testRangeMax) ? X : nullValue<I16u>()), clusterProcessed[X - testRangeMin]);
+        }
+
+        // check frequencies
+        for (I16u X = testRangeMin; X < testRangeMax + 1U; X++)
+        {
+            double pdfXExp = (X < testRangeMax) ? prFunctionInput->pdf(X) : prFunctionInput->pdf(nullValue<I16u>());
+            double pdfXAct = absoluteFrequencies[X - testRangeMin]/static_cast<double>(max-min);
+
+            CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Relative frequencies do not match", pdfXExp, pdfXAct, 5 * pow(10.0, -static_cast<double>(scale/2)));
+        }
     }
 
     void testClusteredValueProviderWithUnsupportedRangeProviderForCardinality()
